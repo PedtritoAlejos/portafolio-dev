6de3fc32b0187bad26760ecdefd6b1a29f891315
UwU <maulenadasmejose@gmail.com>
2021-03-16 17:07:28 -0300

    validacion servidor lista,falta warning html

--- public/assets/mail/index.js
+++ public/assets/mail/index.js
@@ -4,6 +4,7 @@ const router=Router();
 router.post('/send-mail',(req,res)=>{
     const {username, userphone, usermail, usermessage}=req.body;
     res.json({message:'mensaje enviado'});
+    console.log(req.body);
 
 });
 
--- public/assets/mail/validator.js
+++ public/assets/mail/validator.js
@@ -1,8 +1,8 @@
-//delete input if not valid
-
-const { json } = require("express");
+//global validator
+let isInputValid=[false,false,false,false];
 
 //get elements from the DOM
+const nameInput=document.getElementById("name");
 const emailInput=document.getElementById("email");
 const phoneInput=document.getElementById("phone");
 const messageInput=document.getElementById("message");
@@ -16,33 +16,41 @@ messageInput.addEventListener('blur',MessageValidate);
 function NameValidate(){
     const nameRegex=/^[A-Za-z]{2,20}( )?[A-Za-z]{2,20}$/
     if(!nameRegex.test(nameInput.value)){
-        NotValid(nameInput);
+        isInputValid[0]=false;
+        console.log(isInputValid);
     }else{
-        Valid(nameInput);
+        isInputValid[0]=true;
+        console.log(isInputValid);
     }
 }
 function EmailValidate(){
     const emailRegex=/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     if(!emailRegex.test(emailInput.value)){
-        NotValid(emailInput);
+        isInputValid[1]=false;
+        console.log(isInputValid);
     }else{
-        Valid(emailInput);
+        isInputValid[1]=true;
+        console.log(isInputValid);
     }
 }
 function PhoneValidate(){
     const phoneRegex=/^(\+56)?(9{1})(\d{8})$/;
     if(!phoneRegex.test(phoneInput.value)){
-        NotValid(phoneInput);
+        isInputValid[2]=false;
+        console.log(isInputValid);
     }else{
-        Valid(phoneInput);
+        isInputValid[2]=true;
+        console.log(isInputValid);
     }
 }
 function MessageValidate(){
     const messageRegex=/^[a-zA-Z0-9-_.,"'\(\)\ ]{2,150}$/;
     if(!messageRegex.test(messageInput.value)){
-        NotValid(messageInput);
+        isInputValid[3]=false;
+        console.log(isInputValid);
     }else{
-        Valid(messageInput);
+        isInputValid[3]=true;
+        console.log(isInputValid);
     }
 }
 
@@ -56,26 +64,34 @@ function NotValid(input){
     input.classList.remove("valid");
 }
 //enviar mensaje
-function EnviarMensaje(){ 
-    let username = document.getElementById("name").value;
-    let userphone = document.getElementById("phone").value;
-    let usermail = document.getElementById("email").value;
-    let usermessage = document.getElementById("message").value;
-
-    let data={
-        username:username,
-        userphone: userphone,
-        usermail: usermail,
-        usermessage: usermessage
+function IsInputValid(){
+    if(isInputValid.includes(false)){
+        console.log('faltan campos por rellenar');
+    }else{
+        console.log('mesaje enviado');
+        function EnviarMensaje(){ 
+            let username = document.getElementById("name").value;
+            let userphone = document.getElementById("phone").value;
+            let usermail = document.getElementById("email").value;
+            let usermessage = document.getElementById("message").value;
+        
+            let data={
+                username:username,
+                userphone: userphone,
+                usermail: usermail,
+                usermessage: usermessage
+            }
+        
+            $.ajax({
+                type: "POST",
+                url: '/send-mail',
+                data: data,
+                success: function(response){
+                    alert(response.message);
+                },
+                dataType: 'json'
+              });
+        }
+        EnviarMensaje();
     }
-
-    $.ajax({
-        type: "POST",
-        url: '/send-mail',
-        data: data,
-        success: function(response){
-            alert(response.message);
-        },
-        dataType: 'json'
-      });
 }
